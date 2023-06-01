b32b676650492a7fde0948c92b34fc0d0bf481ce
jztimm <jztimm@gmail.com>
2023-05-31 22:54:25 -0400

    Get Frame setup

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import tkinter
 import customtkinter
-from pytube import Youtube
+from pytube import YouTube
+
+# System Settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# Our app frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("Youtube Downloader")
+
+# Run app
+app.mainloop()
